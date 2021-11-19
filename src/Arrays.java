3917eaaa491b22b8aedc9d9df842f65524928098
vikadonika <donika.vika@gmail.com>
2021-11-19 22:46:49 +0200

    init

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+
+    public static void main(String[] args){
+
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,11,12};
+        String[] months= new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+        for (int i = 0; i < 12; i++){
+            System.out.println(nums[i]);
+            System.out.println(months[i]);
+        }
+    }
+}
